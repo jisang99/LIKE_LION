8b46eb83dbf01b75ed645c10b010cde472547014
leejisang <leejisang99@naver.com>
2023-04-04 15:29:19 +0900

    js 과제 완료

--- JSedu.js
+++ JSedu.js
@@ -10,7 +10,6 @@
 //       `
 // console.log(picture);
 
-
 // 2번문제
 
 // const year1 = 2000;
@@ -33,5 +32,57 @@
 // // 윤년이 아니다
 // leapYear(year2);
 
-
 // 3번문제
+
+// const N = 24;
+
+// let blank = '';
+// let star = '';
+
+// for (let i = 1; i <= N; i++) {
+//   star += '*'
+//   for (let j = 0; j < N - i; j++) {
+//     blank += ' ';
+//   }
+//   console.log(blank + star);
+//   blank = '';
+// }
+
+// 4번문제
+
+// const C1 = [5, 50, 50, 70, 80, 100];
+// const C2 = [7, 100, 95, 90, 80, 70, 60, 50];
+// const C3 = [3, 70, 90, 80];
+// const C4 = [3, 70, 90, 81];
+
+// let func = lst => {
+//   const num = lst.shift();
+//   const sum = lst.reduce((acc, curr) => acc + curr, 0)
+//   const mean = sum / num;
+//   let cnt = 0;
+
+//   lst.forEach(e => {
+//     if (mean > e){
+//       cnt += 1;
+//     }
+//   });
+
+//   console.log((cnt / num).toFixed(3));
+// }
+
+// func(C1);
+// func(C2);
+// func(C3);
+// func(C4);
+
+// 5번문제
+
+// const N  = 2;
+
+// let func = n =>{
+//   for (let i = 1; i < 10; i++){
+//     console.log(`${N} * ${i} = ${N*i}`);
+//   }
+// }
+
+// func(N);
