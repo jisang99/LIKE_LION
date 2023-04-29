e0058e88e1c69b823a07f5516b1d9086c33be1bf
leejisang <leejisang99@naver.com>
2023-04-30 00:00:16 +0900

    오른쪽 nav 메뉴 드롭다운 구현 완료

--- pageHive.js
+++ pageHive.js
@@ -31,3 +31,72 @@ circle.addEventListener("mouseout", ()=>{
   circle.style.backgroundColor = "gray";
   circle.style.opacity = "0.5";
 })
+
+
+// 오른쪽 메뉴 a태그 기능 없애버리기 
+const rightNavLinks = document.querySelectorAll('.rightNav a');
+
+rightNavLinks.forEach((rightNavLink) => {
+  rightNavLink.addEventListener('click', function(event) {
+  event.preventDefault(); // 클릭 이벤트를 취소합니다.
+    // 여기에 클릭 이벤트가 발생했을 때 실행할 코드를 추가합니다.
+  });
+})
+
+
+
+// 드롭다운 메뉴 구현하기 위한 코드 (id 부여해서 개별로 드롭다운 메뉴 동작하도록 구현)
+// 사실상 노가다로 구현함
+const subMenu = document.querySelector(".subMenu");
+// 첫번째
+const rightNavMenu1 = document.querySelector("#rightNavMenu1");
+const subMenu1 = document.querySelector("#subMenu1");
+
+let show_sub1 = () =>{
+  subMenu1.style.height="120px";
+}
+
+let close_sub1 = () =>{
+subMenu1.style.height="0px";
+}
+
+// 그냥 단순하게 생각해서 알고리즘 만들면 되더라
+rightNavMenu1.addEventListener("mouseover", show_sub1);
+rightNavMenu1.addEventListener("mouseout", close_sub1);
+subMenu1.addEventListener("mouseover", show_sub1);
+subMenu1.addEventListener("mouseout", close_sub1);
+
+// 두번째
+const rightNavMenu2 = document.querySelector("#rightNavMenu2");
+const subMenu2 = document.querySelector("#subMenu2");
+
+let show_sub2 = () =>{
+  subMenu2.style.height="120px";
+}
+
+let close_sub2 = () =>{
+subMenu2.style.height="0px";
+}
+
+rightNavMenu2.addEventListener("mouseover", show_sub2);
+rightNavMenu2.addEventListener("mouseout", close_sub2);
+subMenu2.addEventListener("mouseover", show_sub2);
+subMenu2.addEventListener("mouseout", close_sub2);
+
+// 네번째
+const rightNavMenu4 = document.querySelector("#rightNavMenu4");
+const subMenu4 = document.querySelector("#subMenu4");
+
+
+let show_sub4 = () =>{
+    subMenu4.style.height="120px";
+}
+
+let close_sub4 = () =>{
+  subMenu4.style.height="0px";
+}
+
+rightNavMenu4.addEventListener("mouseover", show_sub4);
+rightNavMenu4.addEventListener("mouseout", close_sub4);
+subMenu4.addEventListener("mouseover", show_sub4);
+subMenu4.addEventListener("mouseout", close_sub4);
