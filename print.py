9f9ceef8af711f08524e72f90e412f3e4d73fe60
jisang99 <leejisang99@naver.com>
2023-05-04 19:58:12 +0900

    백엔드 첫 세션

---
+++ print.py
@@ -0,0 +1,8 @@
+import requests
+from bs4 import BeautifulSoup
+url = "https://www.naver.com/"
+response = requests.get(url)
+print(response.status_code)
+# print(response.text)
+soup = BeautifulSoup(response.content, "html.parser")
+print(soup.find_all("h2"))
