31c9033cba549ca400f744093cf5491d727e64f7
leejisang <leejisang99@naver.com>
2023-05-18 01:20:51 +0900

    django 블로그에 CRUD 만들기 과제 완료

--- 파이썬 과제.py
+++ 파이썬 과제.py
@@ -3,6 +3,8 @@ from bs4 import BeautifulSoup
 url = "https://www.naver.com/"
 response = requests.get(url)
 print(response.status_code)
-# print(response.text)
+
+print(response.text)
+
 soup = BeautifulSoup(response.content, "html.parser")
 print(soup.find_all("h2"))
