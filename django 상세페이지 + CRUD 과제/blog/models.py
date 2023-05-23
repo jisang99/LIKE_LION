5514e6e9363b5440ff9a2095d1d9f6e327474b9a
jisang99 <leejisang99@naver.com>
2023-05-23 20:27:50 +0900

    피그마 수업했음

--- django 상세페이지 + CRUD 과제/blog/models.py
+++ django 상세페이지 + CRUD 과제/blog/models.py
@@ -5,6 +5,8 @@ class Post(models.Model):
     title = models.CharField(max_length=50)
     pub_date = models.DateTimeField()
     body = models.TextField()
+
+    ## pip install pillow 해줘야한다.
     photo = models.ImageField(upload_to='blog/', null=True, blank=True)
 
     def __str__(self):
